a1d42e56eb767c59e52880c59077ed0e9f23712a
yisier <1340976576@qq.com>
2024-09-24 15:06:58 +0800

    window and linux

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    home: false,
+    title: 'NATNPS 云穿透使用教程',
+    description: 'Just playing around',
+    themeConfig: {
+        sidebar: [
+            '/',
+            '/client/linux'
+        ]
+    }
+
+}
