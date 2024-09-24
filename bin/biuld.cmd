0447cffcba13fed5ffc10932a4a6758209edf575
yisier <1340976576@qq.com>
2024-09-24 15:52:46 +0800

    test

--- bin/biuld.cmd
+++ bin/biuld.cmd
@@ -1,3 +1,15 @@
 cd ..
 # 生成静态文件
 npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git config –global user.name “yisier”
+git config –global user.email “1340976576@qq.com”
+git init
+git add -A
+git commit -m 'deploy'
+
+
+git push -f https://github.com/yisier/natnps.github.io.git master
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     home: false,
+    base: '/',
     title: 'NATNPS 云穿透使用教程',
     description: 'Just playing around',
     themeConfig: {
