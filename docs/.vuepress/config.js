2fa446c254be16046a125bbba2f7f2f29b185efd
yisier <1340976576@qq.com>
2025-01-15 13:30:34 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,13 +2,37 @@ module.exports = {
     home: false,
     base: '/',
     title: 'NATNPS 云穿透使用教程',
-    description: 'Just playing around',
+    description: 'natnps.com NPS云穿透使用教程，帮助手册',
     themeConfig: {
-        sidebar: [
-            '/',
-            '/client/linux',
-            '/client/batch'
-        ]
+        // sidebar: [
+        //     '/',
+        //     '/client/linux',
+        //     '/client/batch'
+        // ]
+        // {
+        sidebar:
+            [
+                {
+                    title: '客户端',
+                    collapsible: true,
+                    children: [
+                        '/client/windows',
+                        '/client/linux',
+                        '/client/batch',
+                    ],
+
+                },
+                {
+                    title: '隧道',
+                    collapsible: true,
+                    children: [
+                        '/tunnel/http',
+                    ],
+
+                }
+            ]
+
+
     }
 
 }
